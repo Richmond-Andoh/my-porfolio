ac855bc578cbc5db86134c9992307f73147eb4b6
Richmond-Andoh <richmondandoh00@gmail.com>
2022-10-21 10:59:29 -0700

    addition of new components

---
+++ src/components/AboutContent.js
@@ -0,0 +1,35 @@
+import "./AboutContentStyle.css";
+import react1 from "../Images/react1.png";
+import react2 from "../Images/react2.avif";
+
+import React from 'react'
+
+const AboutContent = () => {
+  return (
+    <div className="about">
+        <div className="about-container">
+            <div className="left-content">
+                <h1 className="left-heading">Who Am I?</h1>
+                <p>I'm a Freelancer FrontEnd React Developer.
+                    I create responsive secure websites for my clients.
+                </p>
+                <a href="/contact">
+                    <button className="btn-warning" type="button">CONTACT</button>
+                </a>
+            </div>
+            <div className="right-content">
+               <div className="img-container">
+                    <div className="img-top">
+                        <img src={react1} alt="react1-img"/>
+                    </div>
+                    <div className="img-bottom">
+                        <img src={react2} alt="react2-img"/>
+                    </div>
+               </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default AboutContent;
--- src/routes/About.js
+++ src/routes/About.js
@@ -2,12 +2,14 @@ import React from "react";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer"
 import HeroSession2 from "../components/HeroSession2";
+import AboutContent from "../components/AboutContent";
 
 const About = () => {
     return (
         <div>
             <Navbar />
             <HeroSession2 heading="ABOUT." text="I'm a friendly Frontend Developer"/>
+            <AboutContent />
             <Footer />
         </div>
     )
